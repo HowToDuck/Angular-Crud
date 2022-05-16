718432db72f2d27cbcee5714819747625acb9af6
Hugo Tran <006.tran.h@gmail.com>
2022-05-16 15:20:00 -0700

    Model class
    Adding model class to hold object values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEntryComponent } from './create-entry/create-entry.component';
+import { ListEntryComponent } from './list-entry/list-entry.component';
+import { EditEntryComponent } from './edit-entry/edit-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEntryComponent,
+    ListEntryComponent,
+    EditEntryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/entry.model.ts
@@ -0,0 +1,9 @@
+export class Entry {
+  id: string;
+  name: string;
+  current_ep: number;
+  total_ep: number;
+  season: number;
+  rating: number;
+  notes: string;
+}
