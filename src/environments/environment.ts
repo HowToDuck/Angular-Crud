c4accecd43869c090869611fc80db1886d6a7816
Hugo Tran <006.tran.h@gmail.com>
2022-05-16 13:40:51 -0700

    Initializing firebase
    Firebase configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  apiKey: 'AIzaSyB8SyP5sQbhQQ7Rkysbug5VuPJ7cLEhUd0',
+  authDomain: 'angular-crud-watchlist.firebaseapp.com',
+  projectId: 'angular-crud-watchlist',
+  storageBucket: 'angular-crud-watchlist.appspot.com',
+  messagingSenderId: '1084833007219',
+  appId: '1:1084833007219:web:beda57df51797ffa7f0c06',
 };
 
 /*
