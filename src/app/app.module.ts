422044f753730d322c21e8574dd800af1e29fbfc
Hugo Tran <006.tran.h@gmail.com>
2022-05-16 15:46:19 -0700

    Initializing service file, adding imports for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,33 @@ import { CreateEntryComponent } from './create-entry/create-entry.component';
 import { ListEntryComponent } from './list-entry/list-entry.component';
 import { EditEntryComponent } from './edit-entry/edit-entry.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
     CreateEntryComponent,
     ListEntryComponent,
-    EditEntryComponent
+    EditEntryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+export function getBaseUrl() {
+  return document.getElementsByTagName('base')[0].href;
+}
