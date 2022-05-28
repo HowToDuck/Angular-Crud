c584e26190bc073458a977530b0478d5b4887458
Hugo Tran <006.tran.h@gmail.com>
2022-05-28 16:01:25 -0700

    Environment update
    environment.prod.ts is used for build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB8SyP5sQbhQQ7Rkysbug5VuPJ7cLEhUd0',
+    authDomain: 'angular-crud-watchlist.firebaseapp.com',
+    projectId: 'angular-crud-watchlist',
+    storageBucket: 'angular-crud-watchlist.appspot.com',
+    messagingSenderId: '1084833007219',
+    appId: '1:1084833007219:web:beda57df51797ffa7f0c06',
+  },
 };
